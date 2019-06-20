b0ae76a8869d6321d216fb0e2bf4e46c42ec7225
yigegehao <1119082433@qq.com>
2019-06-20 16:40:33 +0800

    del gradu*

--- graduationDesign/毕业设计-肖意/OnLineTest/WebRoot/js/question.js
+++
@@ -1,6 +0,0 @@
-
-function question(id){
-	var question_action = $.trim($("#question_action").val()) +"?subjectId="+ id;
-	
-	 window.location.href = question_action;
-}
--- graduationDesign/毕业设计-肖意/OnLineTest/WebRoot/js/score.js
+++
@@ -1,6 +0,0 @@
-
-function score(id,sid){
-	var score_action = $.trim($("#score_action").val()) +"?subjectId="+ id + "&studentId="+sid;
-	
-	 window.location.href = score_action;
-}
