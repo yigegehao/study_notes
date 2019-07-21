817d25739caf17a0bfe65d4eea20e47ce114c229
yigegehao <1119082433@qq.com>
2019-07-21 10:35:08 +0800

    update

---
+++ web_front_study/nodejs/node-debug/test.js
@@ -0,0 +1,12 @@
+const os =  require('os')
+console.log(os.uptime())
+
+const b = require('./dir/')
+
+//console.log(a.x);
+console.log(b.y);
+let buffer = Buffer.from([1,0x20,3]);
+for(const b of buffer){
+    console.log(b);
+}
+console.log(buffer)
