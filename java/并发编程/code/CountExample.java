e7e1665c46b94792aa79f75ccaebf3846453b7ed
yigegehao <1119082433@qq.com>
2019-07-28 11:50:21 +0800

    'update'

---
+++ java/并发编程/code/CountExample.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class CountExample {
+    private static int threadTotal = 200;
+    private static int clientTotal = 5000;
+    private static int count = 0;
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        for(int index = 0;index < clientTotal;index++){
+            exec.execute(()->{
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                }catch (Exception e){
+
+                }
+            });
+        }
+        exec.shutdown();
+        System.out.println(count);
+    }
+
+    private static void add() {
+        count++;
+    }
+}
